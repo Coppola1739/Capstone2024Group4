a8e4fa6fe79222fd9813cd0a8c962918538e15e0
Jeffrey Emekwue <jemekwu1@my.westga.edu>
2024-01-19 02:39:50 -0500

    Add Basic Video Player Control

--- Group4DesktopApp/Group4DesktopApp/MainWindow.xaml.cs
+++ Group4DesktopApp/Group4DesktopApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Group4DesktopApp.UserControls;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,31 @@ namespace Group4DesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UIElement[] screens;
         public MainWindow()
         {
             InitializeComponent();
+            this.screens = new UIElement[] { this.pdfViewer, this.videoPlayer };
+        }
+
+        private void showScreen(UIElement screen)
+        {
+            for (int i = 0; i < this.screens.Length; i++)
+            {
+                var screenElement = this.screens[i];
+                if(screenElement == screen)
+                {
+                    screenElement.Visibility = Visibility.Visible;
+                } else
+                {
+                    screenElement.Visibility = Visibility.Collapsed;
+                    if(screenElement is VideoPlayer)
+                    {
+                        VideoPlayer vp = (VideoPlayer)screenElement;
+                        vp.videoFrame.LoadedBehavior = MediaState.Stop;
+                    }
+                }
+            }
         }
 
         private void btnOpenPDF_Click(object sender, RoutedEventArgs e)
@@ -33,13 +58,15 @@ namespace Group4DesktopApp
             bool? result = dialog.ShowDialog();
             if (result == true)
             {
+                this.showScreen(this.pdfViewer);
                 this.pdfViewer.Navigate(dialog.FileName);
             }
         }
 
         private void btnOpenVideo_Click(object sender, RoutedEventArgs e)
         {
-
+            this.videoPlayer.loadVideo();
+            this.showScreen(this.videoPlayer);
         }
     }
 }
---
+++ Group4DesktopApp/Group4DesktopApp/UserControls/VideoPlayer.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Group4DesktopApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for VideoPlayer.xaml
+    /// </summary>
+    public partial class VideoPlayer : UserControl
+    {
+        private bool IsPlaying = false;
+        private bool IsUserDraggingSlider = false;
+
+        private readonly DispatcherTimer Timer = new() { Interval = TimeSpan.FromSeconds(0.1) };
+        public VideoPlayer()
+        {
+            InitializeComponent();
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        public void loadVideo()
+        {
+            OpenFileDialog MediaOpenDialog = new()
+            {
+                Title = "Open a media file",
+                Filter = "Media Files (*.mp4, *.wmv, *.mpeg, *.avi)|*.mp4;*.wmv;*.mpeg;*.avi"
+            };
+            if (MediaOpenDialog.ShowDialog() == true)
+            {
+                this.videoFrame.LoadedBehavior = MediaState.Pause;
+                this.videoFrame.Source = new Uri(MediaOpenDialog.FileName);
+                this.IsPlaying = false;
+            }
+
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (this.videoFrame.Source != null && this.videoFrame.NaturalDuration.HasTimeSpan && !IsUserDraggingSlider)
+            {
+                this.progressSlider.Maximum = this.videoFrame.NaturalDuration.TimeSpan.TotalSeconds;
+                this.progressSlider.Value = this.videoFrame.Position.TotalSeconds;
+            }
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.videoFrame?.Source != null)
+            {
+                this.videoFrame.LoadedBehavior = MediaState.Play;
+                this.IsPlaying = true;
+            }
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.IsPlaying)
+                this.videoFrame.LoadedBehavior = MediaState.Pause;
+        }
+
+        private void Slider_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            this.IsUserDraggingSlider = true;
+        }
+
+        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            this.IsUserDraggingSlider = false;
+            this.videoFrame.Position = TimeSpan.FromSeconds(this.progressSlider.Value);
+        }
+
+        private void progressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.lblTimePosition.Content = TimeSpan.FromSeconds(this.progressSlider.Value).ToString(@"hh\:mm\:ss");
+        }
+    }
+}
