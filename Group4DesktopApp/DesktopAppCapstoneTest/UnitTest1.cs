33a6206bb1194370f723559748fb0d5c9d7c8568
Jeffrey Emekwue <jemekwu1@my.westga.edu>
2024-01-22 05:47:27 -0500

    Adds nUnit test and coverage report for DesktopApp

---
+++ Group4DesktopApp/DesktopAppCapstoneTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using Group4DesktopApp;
+using Group4DesktopApp.UserControls;
+
+namespace DesktopAppNUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        
+        }
+    }
+}
